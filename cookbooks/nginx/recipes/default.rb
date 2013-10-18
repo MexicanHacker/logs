97a0bc4f6b482a6497d0b2697146073da61a208d
Vlad Akilov <vakilov@tacitknowledge.com>
2013-10-18 17:15:38 -0400

    Removed old chef-logstash cookbook, added nginx cookbook.

---
+++ cookbooks/nginx/recipes/default.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2012, Tacit Knowledge, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+package "nginx" do
+  version node[:nginx][:version]
+end
+
+service "nginx" do
+  supports :status => true, :restart => true
+  action :nothing
+end
+
+service "nginx" do
+  action [ :enable, :start ]
+end
